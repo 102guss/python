d649b21b4e5f6fb0e7e954c9f11a205ce9847dee
102guss <brown_321@live.com>
2023-01-13 17:52:37 +0100

    Update marco2.py
    Marco terminado

--- figurasCiclos/marco2.py
+++ figurasCiclos/marco2.py
@@ -4,7 +4,10 @@ rango = range(numero)
 
 for j in  rango:
     for i in rango:
-        if (j==0) || (j==numero-1) || (i==0) || (i==numero-1):
-       
-    
+        if (j==0) or (j==numero-1) or (i==0) or (i==numero-1):
+         print(" *",end="")
+        else:
+         print("  ",end="")
+    print("")#salto de linea
+        
 
