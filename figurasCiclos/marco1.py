34eed6d159d3aa69ed86cb32d79e81f157e0de37
102guss <brown_321@live.com>
2023-01-13 15:51:42 +0100

    tercer commit

--- figurasCiclos/marco1.py
+++ figurasCiclos/marco1.py
@@ -3,5 +3,15 @@ numero = int(x)
 rango = range(numero)
 
 for i in rango:
-    print("*",end = "")
-    
+    print(" *",end = "")
+
+for j in range(numero-2):
+    print("  ")#espacio en blanco
+    print(" *",end="")
+    for k in range(numero-2):
+        print("  ",end="")#dos espacios en blanco
+    print(" *",end="")
+print("  ")#espacio en blanco
+
+for  m in rango:
+    print(" *",end="")
