612665f085c7ea8dabf00b3b5009eade483c89fa
102guss <brown_321@live.com>
2023-01-13 16:00:08 +0100

    Create marco2.py
    commit1

---
+++ figurasCiclos/marco2.py
@@ -0,0 +1,10 @@
+x = input("Introduce el numero de renglones del marco: ")
+numero = int(x)
+rango = range(numero)
+
+for j in  rango:
+    for i in rango:
+        if (j==0) || (j==numero-1) || (i==0) || (i==numero-1):
+       
+    
+
