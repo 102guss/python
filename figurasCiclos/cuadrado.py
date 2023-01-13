5e9bec16415f5ef72a9c6b44ef71f160886a022e
102guss <brown_321@live.com>
2023-01-13 07:37:25 +0100

    commit 2

--- figurasCiclos/cuadrado.py
+++ figurasCiclos/cuadrado.py
@@ -1,1 +1,9 @@
-print("Esto es un cuadrado")
+x=input("introduce el numero de renglones del cuadardo: ")
+numero = int(x)
+rango = range(numero)
+for j in rango:
+    for i  in rango:
+        print( " *",end="")
+    print("") #esto es un salto de linea de parte de pyton: es como un println en java
+    
+
